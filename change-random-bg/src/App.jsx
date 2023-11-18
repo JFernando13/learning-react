ca66b87a1a790bfa79ce75badea61e1e9570e468
JFernando13 <juan_fer-13@hotmail.com>
2023-11-18 06:06:55 -0500

    complete change background project

--- change-random-bg/src/App.jsx
+++ change-random-bg/src/App.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("#000000")
+  const [bgColor, setBgColor] = useState(color)
+
   const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
+    const letters = "0123456789abcdef";
     let color = "#";
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
@@ -11,18 +14,46 @@ function App() {
     return color;
   }
 
-  const changeBgColor = (element) => element.style.backgroundColor = getRandomColor() 
+  const changeBgColor = () => {
+    const randomColor = getRandomColor()
+
+    window.document.body.style.backgroundColor = randomColor
+    setColor(randomColor)
+    setBgColor(adjustColor(randomColor, 1.8))
+  }
+
+  const adjustColor = (color, factor) => {
+    const hexToRgb = (hex) => {
+      hex = hex.replace(/^#/, '');
+    
+      const bigint = parseInt(hex, 16);
+      const r = (bigint >> 16) & 255;
+      const g = (bigint >> 8) & 255;
+      const b = bigint & 255;
+    
+      return [r, g, b];
+    };
+    
+    const rgb = hexToRgb(color)
+
+    const adjustColor = rgb.map(component => {
+      return Math.min(Math.round(component * factor), 255).toString(16).padStart(2, '0')
+    });
+
+    return "#" + adjustColor.join('')
+  }
 
   useEffect(() => {
-    changeBgColor(window.document.body)
-
-    return () => window.alert('Adios mi amigo')
+    changeBgColor()
   }, [])
 
   return (
-    <>
-      <button onClick={() => changeBgColor(window.document.body)}>Change Background</button>
-    </>
+      <button 
+        onClick={changeBgColor} 
+        style={{color, backgroundColor: bgColor}}
+      >
+        Change Background
+      </button>
   )
 }
 
